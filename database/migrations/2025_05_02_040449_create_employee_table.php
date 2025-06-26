f0df9b0179b0e7bd4297d8fa04b1159543c5d229
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2025-06-26 16:21:18 +0700

    [MODIFY] change the table name from employees to users

--- database/migrations/2025_05_02_040449_create_employee_table.php
+++ database/migrations/2025_05_02_040449_create_employee_table.php
@@ -67,7 +67,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('employees');
+        Schema::dropIfExists('users');
         Schema::dropIfExists('positions');
         Schema::dropIfExists('departments');
         Schema::dropIfExists('settings');
