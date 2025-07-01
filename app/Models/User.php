2c0140869c569118603a668597f40654e6e50504
Yanasorn Wongpakdee <119282971+yanasorn2003@users.noreply.github.com>
2025-07-01 16:41:30 +0700

    -send reset password link through mail -config .env APP_URL='http://127.0.0.1:8000', APP_NAME= 'MSU Public Health Personnel Performance Evaluation System' - use mailable

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,16 +3,19 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Notifications\CustomResetPassword;
+use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Auth\Passwords\CanResetPassword as CanResetPasswordTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements CanResetPassword
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles, HasApiTokens;
+    use HasFactory, Notifiable, HasRoles, HasApiTokens,CanResetPasswordTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -65,5 +68,10 @@ class User extends Authenticatable
 
     public function department(){
         return $this->belongsTo(Department::class);
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new CustomResetPassword($token));
     }
 }
